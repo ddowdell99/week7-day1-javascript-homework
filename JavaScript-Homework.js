db1d5e1b4872d133ac25c170835660164cea4853
Diante Dowdell <diante.dowdell99@gmail.com>
2022-11-01 22:56:45 -0500

    added codewars problems"

--- JavaScript-Homework.js
+++ JavaScript-Homework.js
@@ -41,3 +41,78 @@ console.log(answer2)
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
+
+/*
+Get the Middle Chracter
+
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+*/
+
+function getMiddle(s)
+{
+  if (s.length%2 === 0){
+    return s[(s.length/2)-1]+s[s.length/2]
+  } else {
+    return s[Math.floor(s.length/2)]
+  }
+}
+
+console.log(getMiddle('Diante'))
+
+/*
+You're a square!
+
+A square of squares
+
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+Task
+
+Given an integral number, determine if it's a square number:
+
+    In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+Examples
+
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+
+
+*/
+
+var isSquare = function(n){
+    if (n < 0){
+      return false;
+    } if (n**(1/2) % 1 === 0){
+      return true
+    } else {
+      return false
+    }      
+  }
+
+console.log(isSquare(25))
